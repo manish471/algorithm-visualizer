8e62a8310c4489042871f24be9f7bc62371dcd21
maxchougule99@gmail.com <maxchougule99@gmail.com>
2024-02-12 20:28:27 +0530

    build added

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+
 const corsOptions = {
     origin:(origin, cb) => {
         return cb(null, true);
