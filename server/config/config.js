97bdfdc445ab248bc4eca85421d4ede430b766bf
MAChougule <manish.chougule@xecurify.com>
2024-02-12 16:29:18 +0530

    google oauth creds changed!

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ const config = {
     db_local: 'mongodb://localhost:27017/algorithms-visualizer',
     db_cloud:'mongodb+srv://avuser:avdbpwd@algorithm-visualizer.cboto.mongodb.net/algorithm-visualizer?retryWrites=true&w=majority',
     frontEndServer:'https://algorithm-visualizer-livid.vercel.app',
-    googleClientID:'945383315679-16s0g0par0bn36au8mbver0jemv959od.apps.googleusercontent.com',
-    googleClientSecret:'zTlp6qLOF-fb1C1f-7gMLrJe',
+    googleClientID:'920749689036-avuoaed91ebqp60oj58fa0stmgtv0khk.apps.googleusercontent.com',
+    googleClientSecret:'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',
     githubClientID:'82cf8a0540501bb4d44f',
     githubClientSecret:'fbcf18f24515d701170323121c844428e1a43572'
 }
