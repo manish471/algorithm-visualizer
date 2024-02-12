ced4192cd117f93c5d9a8505d6afd088a217d5ba
maxchougule99@gmail.com <maxchougule99@gmail.com>
2024-02-12 22:26:23 +0530

    js content security changes

--- index.js
+++ index.js
@@ -27,7 +27,8 @@ app.use(
     helmet.contentSecurityPolicy({
         useDefaults: true,
         directives: {
-          "img-src": ["'self'", "https: data:"]
+          "img-src": ["'self'", "https: data:"],
+          "script-src-elem":["'self'", "https: data:"],
         }
       })
 );
