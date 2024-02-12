82af2652659e663c16e876eb3541f2ba36fc279f
maxchougule99@gmail.com <maxchougule99@gmail.com>
2024-02-12 20:41:28 +0530

    bug fix in routes file

--- server/routes/index.js
+++ server/routes/index.js
@@ -40,8 +40,8 @@ routes.use( (err, req, res, next) => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
+  routes.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  routes.get("*", (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
           if(err) {
               res.status(500).send(err)
